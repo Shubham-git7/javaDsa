267ada2552d7690b2adf004f6ff433a625f22cc5
Shubham <gulhane383@gmail.com>
2024-07-29 13:38:27 +0530

    max profit in stock

--- Arrays/arraysCC.java
+++ Arrays/arraysCC.java
@@ -236,8 +236,56 @@ import java.util.*;
 
 //! Trapping Rain water problem
 
+// public class arraysCC {
+//     public static int trappedRainWater(int height[]){
+//         int n = height.length;
+//         //Calculate left max-boundary
+//         int leftMax[] = new int[n];
+//         leftMax[0] = height[0];
+//         for (int i = 1; i < n; i++) {
+//             leftMax[i] = Math.max(height[i] , leftMax[i - 1 ]);
+//         }
+//         //Caluclate rightMax boundary 
+//         int rightMax[] = new int[n];
+//         rightMax[n - 1] = height[n - 1];
+//         for (int i = n - 2; i>=0; i--){
+//             rightMax[i] = Math.max(height[i] , rightMax[i+1]);
+//         }
+//         int trappedWater = 0;
+//         //loop
+//         for(int i=0; i<n; i++){
+//             //min of leftMax and RightMax boundary
+//             int waterLevel = Math.min(leftMax[i], rightMax[i]);
+//             //Trapped water formula
+//             trappedWater += waterLevel - height[i]; 
+//         }
+//         return trappedWater;
+//     }
+//     public static void main(String[] args) {
+//         int height[] = {4 , 2 , 0, 6, 3, 2, 5};
+//       System.out.println(trappedRainWater(height));
+//     }
+// }
+
+//! Best time to sell and buy stock
+
 public class arraysCC {
+    public static int BuyAndSellStocks(int price[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < price.length; i++) {
+            if (buyPrice < price[i]) {
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = price[i];
+            }
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
-        
+        int price[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("max profit is " + BuyAndSellStocks(price));
     }
 }
