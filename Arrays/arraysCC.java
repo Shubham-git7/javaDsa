b794b338cca0dbdeb2b7d94dd01f84ee4274caa1
Shubham <gulhane383@gmail.com>
2024-07-30 23:04:56 +0530

    roated array kehy find problem doing

--- Arrays/arraysCC.java
+++ Arrays/arraysCC.java
@@ -267,25 +267,72 @@ import java.util.*;
 //     }
 // }
 
-//! Best time to sell and buy stock
+ //! Best time to sell and buy stock
+
+// public class arraysCC {
+//     public static int BuyAndSellStocks(int price[]) {
+//         int buyPrice = Integer.MAX_VALUE;
+//         int maxProfit = 0;
+//         for (int i = 0; i < price.length; i++) {
+//             if (buyPrice < price[i]) {
+//                 int profit = price[i] - buyPrice;
+//                 maxProfit = Math.max(maxProfit, profit);
+//             } else {
+//                 buyPrice = price[i];
+//             }
+//         }
+//         return maxProfit;
+//     }
+
+//     public static void main(String[] args) {
+//         int price[] = { 7, 1, 5, 3, 6, 4 };
+//         System.out.println("max profit is " + BuyAndSellStocks(price));
+//     }
+// }
+
+
+//! There is an integer array nums sorted in ascending order (with distinct values).
+//! pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], 
+//! Prior to being passed to your function, nums is possibly rotated at an unknown 
+//! nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For 
+//! example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and 
+//! become [4,5,6,7,0,1,2].
+//! Given the array nums after the possible rotation and an integer target, return the 
+//! index of target if it is in nums, or -1 if it is not in nums.
+//! You must write an algorithm with O(log n) runtime complexity
+
 
 public class arraysCC {
-    public static int BuyAndSellStocks(int price[]) {
-        int buyPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        for (int i = 0; i < price.length; i++) {
-            if (buyPrice < price[i]) {
-                int profit = price[i] - buyPrice;
-                maxProfit = Math.max(maxProfit, profit);
-            } else {
-                buyPrice = price[i];
-            }
-        }
-        return maxProfit;
-    }
+    public static int roated_Array_Search(int num[], int target){
+        int left = 0; 
+        int right = num.length - 1;
+        while(left <= right){
+            int mid =left + (right - left) / 2; //Prevent overflow
 
-    public static void main(String[] args) {
-        int price[] = { 7, 1, 5, 3, 6, 4 };
-        System.out.println("max profit is " + BuyAndSellStocks(price));
+            if(num[mid] == target){
+                return mid;
+            }
+            //Determined which part of array is sorted
+            if(num[left] <= num[mid]){//left part is sorted
+                if(num[left] <= target && target < num[mid]){
+                    right = mid - 1;
+                }else{
+                    left = left + 1; 
+                }
+            }else{// right part is sorted   
+                if(num[mid] < target && target < num[right]){
+                    left = mid + 1; //target  is in the left part
+                }else{
+                    right = mid - 1; //target is in the right part 
+                }
+            }
+        } 
+        //if target is not found 
+        return -1;
     }
+public static void main(String[] args) {
+    int num[] = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    System.out.println(roated_Array_Search(num, target));
+}
 }
