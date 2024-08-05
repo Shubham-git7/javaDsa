11c5c860ba48f2d52d77f737f86dcb5ec13eb2f8
Shubham <gulhane383@gmail.com>
2024-08-05 15:10:49 +0530

    2d array lec complete

--- 2D_Array/Matrices.java
+++ 2D_Array/Matrices.java
@@ -57,55 +57,113 @@ import java.util.*;
 //     }
 // }
 
-//!  Spiral matrix 
+// //!  Spiral matrix 
+
+// public class Matrices {
+//     public static void Spiral(int num[][]) {
+//         int startRow = 0;
+//         int startCol = 0;
+//         int endCol = num[0].length - 1;
+//         int endRow = num.length - 1;
+
+//         while (startRow <= endRow && startCol <= endCol) {
+//             // Top
+//             for (int j = startCol; j <= endCol; j++) {
+//                 System.out.print(num[startRow][j]);
+//             }
+
+//             // Right
+//             for (int i = startRow + 1; i <= endRow; i++) {
+//                 System.out.print(num[i][endCol]);
+//             }
+//             ;
+//             // Bottom
+//             for (int j = endCol - 1; j >= startCol; j--) {
+//                 if (startRow == endRow) {
+//                     break;
+//                 }
+//                 System.out.print(num[endRow][j]);
+//             }
+//             // left
+//             for (int i = endRow - 1; i >= startRow + 1; i--) {
+//                 if (startCol == endCol) {
+//                     break;
+//                 }
+//                 System.out.print(num[i][startCol]);
+//             }
+
+//             startRow++;
+//             endCol--; 
+//             endRow--;
+//             startCol++;
+
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//         int num[][] = { { 1, 2, 3, 4 },
+//                 { 5, 6, 7, 8 },
+//                 { 9, 10, 11, 12 },
+//                 { 14, 15, 16, 17 } };
+//         Spiral(num);
+//     }
+// }
+
+//! Diagonal Matrix sum 
+
+// public class Matrices {
+//     public static int DiagonalSum(int num[][]) {
+//         int sum = 0;
+//         for (int i = 0; i < num.length; i++) {
+//             // primary diagonal
+//             sum += num[i][i];
+//             if (i != num.length - i - 1);
+//             // secoundary diagnol
+//             sum += num[i][num.length - 1 - i];
+//         }
+//         System.out.println(sum);
+//         return sum;
+//     }
+//     public static void main(String[] args){
+//         int num[][] = { { 1, 2, 3, 4 },
+//                 { 5, 6, 7, 8 },
+//                 { 9, 10, 11, 12 },
+//                 { 13, 14, 15, 16 } };
+//         DiagonalSum(num);
+//     }
+// }
+
+//! Search in sorted matrix
 
 public class Matrices {
-    public static void Spiral(int num[][]) {
-        int startRow = 0;
-        int startCol = 0;
-        int endCol = num[0].length - 1;
-        int endRow = num.length - 1;
+    public static boolean sortedMatrix(int num[][], int key){
+        int row=0;
+        int col = num[0].length -1;
 
-        while (startRow <= endRow && startCol <= endCol) {
-            // Top
-            for (int j = startCol; j <= endCol; j++) {
-                System.out.print(num[startRow][j]);
+        while(row<num.length && col >=0){
+            if(num[row][col] == key){
+                System.out.println("the key found at index : " + row +","+ col);
+                return true;
             }
-
-            // Right
-            for (int i = startRow + 1; i <= endRow; i++) {
-                System.out.print(num[i][endCol]);
+            else if(key <= num[row][col]){
+                col--;
             }
-            ;
-            // Bottom
-            for (int j = endCol - 1; j >= startCol; j--) {
-                if (startRow == endRow) {
-                    break;
-                }
-                System.out.print(num[endRow][j]);
+            else{
+                row++;
             }
-            // left
-            for (int i = endRow - 1; i >= startRow + 1; i--) {
-                if (startCol == endCol) {
-                    break;
-                }
-                System.out.print(num[i][startCol]);
-            }
-
-            startRow++;
-            endCol--;
-            endRow--;
-            startCol++;
-
         }
-        System.out.println();
+        System.out.println("key not fouund");
+        return false;
     }
-
     public static void main(String[] args) {
         int num[][] = { { 1, 2, 3, 4 },
                 { 5, 6, 7, 8 },
                 { 9, 10, 11, 12 },
-                { 14, 15, 16, 17 } };
-        Spiral(num);
+                { 13, 14, 15, 16 } };
+
+        int key = 11;
+        
+         sortedMatrix(num, key);
     }
 }
